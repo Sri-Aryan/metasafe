22bec0b6f4164082215b4cb9cdbf654df5bc8bc5
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2026-01-26 21:47:34 +0530

    feat: app router

---
+++ lib/app_router.dart
@@ -0,0 +1,28 @@
+// lib/app_router.dart
+import 'package:go_router/go_router.dart';
+
+import 'features/home_screen.dart';
+import 'features/onboarding/onboarding_screen.dart';
+import 'features/splash/splash_screen.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/splash',
+  routes: [
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) => SplashScreen(),
+    ),
+    GoRoute(
+      path: '/onboarding',
+      builder: (context, state) => OnboardingScreen(),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => HomeScreen(),
+    ),
+  ],
+  redirect: (context, state) {
+    // Redirect to onboarding if not completed
+    return null;
+  },
+);
